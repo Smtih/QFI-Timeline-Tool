19828475775ceb379c801ac49681572bc3412c97
David Smith <smith.w.da@gmail.com>
2019-07-07 22:22:15 +1000

    Improve places search useability

--- src/App/components/SideBar/components/PlacesSearch/PlacesSearch.tsx
+++ src/App/components/SideBar/components/PlacesSearch/PlacesSearch.tsx
@@ -30,6 +30,7 @@ class PlacesSearch extends React.Component<Props, State> {
         debounce={500}
         value={this.state.address}
         onChange={this.handleChange}
+        searchOptions={searchOptions}
         {...rest}
       >
         {({ getInputProps, suggestions, loading }) => {
@@ -42,7 +43,14 @@ class PlacesSearch extends React.Component<Props, State> {
 
           return (
             <Select
+              menuIsOpen={options.length > 0}
+              filterOption={() => true}
               onInputChange={value => onChange({ target: { value } })}
+              components={{
+                DropdownIndicator: () => null,
+                IndicatorSeparator: () => null
+              }}
+              placeholder={"Search"}
               options={options}
               onChange={(value: any) => {
                 this.handleSelect(value.label);
@@ -54,6 +62,15 @@ class PlacesSearch extends React.Component<Props, State> {
     );
   }
 }
+
+const google = (window as any).google;
+const searchOptions = {
+  location: new google.maps.LatLng(-37, 144),
+  radius: 5000,
+  componentRestrictions: {
+    country: "AU"
+  }
+};
 
 const StyledPlacesAutocomplete = styled(PlacesAutocomplete)`
   display: flex;
