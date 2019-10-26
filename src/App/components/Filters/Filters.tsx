35a2b2a2590b94bbf8f2a2561be1e23d9b754b4c
David Smith <smith.w.da@gmail.com>
2019-10-26 16:56:31 +1100

    Start and end date on filter

--- src/App/components/Filters/Filters.tsx
+++ src/App/components/Filters/Filters.tsx
@@ -3,11 +3,15 @@ import moment from "moment";
 import styled from "styled-components";
 import Slider from "@material-ui/core/Slider";
 import { useGlobal } from "reactn";
+import { Typography } from "@material-ui/core";
 
 function Filters({ ...rest }) {
   const [currentDate, setCurrentDate] = useGlobal("currentDate");
   return (
     <Container {...rest}>
+      <Typography style={{ paddingRight: 16, textAlign: "right" }}>
+        {formatDate(startDate)}
+      </Typography>
       <Slider
         value={moment(currentDate).unix()}
         onChange={(event, value) =>
@@ -17,15 +21,24 @@ function Filters({ ...rest }) {
         min={startDate}
         max={endDate}
       />
+      <Typography style={{ paddingLeft: 16, textAlign: "left" }}>
+        {formatDate(endDate)}
+      </Typography>
     </Container>
   );
+}
+
+function formatDate(date: number): string {
+  return moment.unix(date).format("DD/MM/YYYY hh:mma");
 }
 
 const startDate = moment("2019-05-12T10:00:00.000Z").unix();
 const endDate = moment("2019-05-13T22:00:00.000Z").unix();
 
 const Container = styled.div`
-  padding: 30px;
+  display: flex;
+  flex-direction: row;
+  padding: 16px;
 `;
 
 export { Filters };
