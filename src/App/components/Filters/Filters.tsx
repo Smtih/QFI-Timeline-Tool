9167ff26c37ca2cb412c1f6b0089118d06f04828
David Smith <smith.w.da@gmail.com>
2019-10-26 19:29:33 +1100

    Add Current date

--- src/App/components/Filters/Filters.tsx
+++ src/App/components/Filters/Filters.tsx
@@ -7,29 +7,35 @@ import { Typography } from "@material-ui/core";
 
 function Filters({ ...rest }) {
   const [currentDate, setCurrentDate] = useGlobal("currentDate");
+  const unixDate = moment(currentDate).unix();
   return (
     <Container {...rest}>
-      <Typography style={{ paddingRight: 16, textAlign: "right" }}>
-        {formatDate(startDate)}
-      </Typography>
-      <Slider
-        value={moment(currentDate).unix()}
-        onChange={(event, value) =>
-          setCurrentDate(moment.unix(value as number).toISOString())
-        }
-        step={3600}
-        min={startDate}
-        max={endDate}
-      />
-      <Typography style={{ paddingLeft: 16, textAlign: "left" }}>
-        {formatDate(endDate)}
-      </Typography>
+      <Row>
+        <Typography>{formatDate(unixDate)}</Typography>
+      </Row>
+      <Row>
+        <Typography style={{ paddingRight: 16, textAlign: "right" }}>
+          {formatDate(startDate)}
+        </Typography>
+        <Slider
+          value={unixDate}
+          onChange={(event, value) =>
+            setCurrentDate(moment.unix(value as number).toISOString())
+          }
+          step={3600}
+          min={startDate}
+          max={endDate}
+        />
+        <Typography style={{ paddingLeft: 16, textAlign: "left" }}>
+          {formatDate(endDate)}
+        </Typography>
+      </Row>
     </Container>
   );
 }
 
 function formatDate(date: number): string {
-  return moment.unix(date).format("DD/MM/YYYY hh:mma");
+  return moment.unix(date).format("DD/MM/YYYY hh:mm a");
 }
 
 const startDate = moment("2019-05-12T10:00:00.000Z").unix();
@@ -37,8 +43,17 @@ const endDate = moment("2019-05-13T22:00:00.000Z").unix();
 
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
   padding: 16px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  justify-content: center;
 `;
 
 export { Filters };
