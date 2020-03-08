b889d38a287b3ae98879e45b555cde651d06336b
David Smith <smith.w.da@gmail.com>
2020-03-08 15:16:01 +1100

    Updates

---
+++ precache-manifest.a2b63811a91b6af728013c4288a63d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78eacdbc7f3a5d4d2dbc9160bdcfdff5",
+    "url": "/QFI-Timeline-Tool/index.html"
+  },
+  {
+    "revision": "136f8ebd702180a6dd39",
+    "url": "/QFI-Timeline-Tool/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5fe838be4ce5f2353c1b",
+    "url": "/QFI-Timeline-Tool/static/js/2.f273492c.chunk.js"
+  },
+  {
+    "revision": "9303cb94077bfb11243c892b33dea1e2",
+    "url": "/QFI-Timeline-Tool/static/js/2.f273492c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136f8ebd702180a6dd39",
+    "url": "/QFI-Timeline-Tool/static/js/main.cdf23ac4.chunk.js"
+  },
+  {
+    "revision": "00c2db46060ebb6d0874",
+    "url": "/QFI-Timeline-Tool/static/js/runtime-main.557ea4ca.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/QFI-Timeline-Tool/precache-manifest.034bea11dee648c25bbe76f8a6e1e3eb.js"
+  "/QFI-Timeline-Tool/precache-manifest.a2b63811a91b6af728013c4288a63d0d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/QFI-Timeline-Tool/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
