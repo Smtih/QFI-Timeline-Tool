2d55392d862cc5c9c2e2dfbb5cd54dede519fc14
David Smith <smith.w.da@gmail.com>
2020-08-02 15:05:23 +1000

    Add info window

--- src/App/components/Map/Map.tsx
+++ src/App/components/Map/Map.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import moment from "moment";
 import styled from "styled-components";
-import { useGlobal } from "reactn";
-import { Map as GoogleMap, Circle, Marker } from "google-maps-react";
+import { useGlobal, useState } from "reactn";
+import {
+  Map as GoogleMap,
+  Circle,
+  Marker,
+  InfoWindow
+} from "google-maps-react";
 
 interface Props {}
+
+interface Info {
+  location: google.maps.LatLngLiteral;
+  firstLine: string;
+  secondLine: string;
+}
 
 function Map({ ...rest }: Props) {
   const [currentPosition, setCurrentPosition] = useGlobal("currentPosition");
@@ -14,6 +25,8 @@ function Map({ ...rest }: Props) {
   const [currentDate] = useGlobal("currentDate");
   const [defaultZoom] = useGlobal("defaultZoom");
   const [defaultCenter] = useGlobal("defaultCenter");
+
+  const [info, setInfo] = useState<Info>();
 
   const eligibleSuspects = suspects.filter(
     ({ startTime, endTime, visible }) =>
@@ -34,30 +47,55 @@ function Map({ ...rest }: Props) {
         center={currentPosition}
         onDragend={() => setCurrentPosition(undefined)}
       >
-        {savedAddresses.map(({ firstLine, location }, i) => (
+        {savedAddresses.map(({ firstLine, secondLine, location }, i) => (
           <Marker
+            onClick={() => {
+              setInfo({ location, firstLine, secondLine });
+            }}
             key={i}
             title={firstLine}
             icon={"http://maps.google.com/mapfiles/ms/icons/red-dot.png"}
             position={location}
           />
         ))}
-        {eligibleSuspects.map(({ location, radius, color }, i) => (
-          <Circle
-            key={i}
-            center={location}
-            radius={radius}
-            fillColor={color}
-            strokeColor={color}
-          />
-        ))}
+        {eligibleSuspects.map(
+          ({ location, radius, color, name, startTime, endTime }, i) => (
+            <Circle
+              onClick={() => {
+                setInfo({
+                  location,
+                  firstLine: name,
+                  secondLine: `${startTime} - ${endTime}`
+                });
+              }}
+              key={i}
+              center={location}
+              radius={radius}
+              fillColor={color}
+              strokeColor={color}
+            />
+          )
+        )}
         {searchedAddress && (
           <Marker
+            onClick={() => {
+              setInfo(searchedAddress);
+            }}
             title={searchedAddress.firstLine}
             icon={"http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}
             position={searchedAddress.location}
           />
         )}
+        <InfoWindow
+          visible={!!info}
+          position={info?.location}
+          onClose={() => setInfo(undefined)}
+        >
+          <div>
+            <p>{info?.firstLine}</p>
+            <p>{info?.secondLine}</p>
+          </div>
+        </InfoWindow>
       </GoogleMap>
     </Container>
   );
